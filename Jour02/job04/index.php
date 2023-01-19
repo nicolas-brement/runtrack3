e1a22527187415b6793dd6ed8a1679022c0e8b3d
nicolas-brement <114388303+nicolas-brement@users.noreply.github.com>
2023-01-19 13:15:28 +0100

    Ajout du job04 jour02
    Créer un textarea qui aura pour but d'ajouter chaque lettre saisit dans la zone de texte si le focus n'est pas sur le texturea et ajouté deux fois cette lettre si le focus se trouve sur le textearea.

---
+++ Jour02/job04/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>runtrack3</title>
+  <link rel="stylesheet" href="">
+</head>
+
+<header>
+</header>
+
+<main class="container">
+<body>
+<textarea id="keylogger"></textarea> 
+</body>
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="script.js"></script>
