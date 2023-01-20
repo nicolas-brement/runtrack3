ff299a5bc9147ebd47941fe73842402e05541144
nicolas-brement <114388303+nicolas-brement@users.noreply.github.com>
2023-01-20 11:46:27 +0100

    Ajout du job05 du jour02
    De la même façon qu’une barre de chargement, j'ai créer un footer dont la couleur évolue en fonction du pourcentage de scrolling.

---
+++ Jour02/job05/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>runtrack3</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<header>
+</header>
+
+<main class="container">
+<body>
+
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+<footer></footer>
+
+<script src="script.js"></script>
+
